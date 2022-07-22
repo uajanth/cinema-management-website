17c0940a34b132fafc6bf16048e53c7cb7275a62
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-07-21 20:30:20 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ["imdb-api.com", "drive.google.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
