759192853a547d5c63f9d716a78359635d47161d
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-07-22 13:35:53 -0400

    add header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,9 @@ import styles from "./Header.module.scss";
 export default function Header({ text, color }) {
 	return (
 		<div className={styles.container}>
-			<div className={styles.marker} style={{ backgroundColor: "red" }} />
-			<div className={styles["header-title"]}>
-				<h2 style={{ margin: 0, fontWeight: 600 }}>Showtimes &amp; Tickets</h2>
+			<div className={styles.marker} style={{ backgroundColor: color }} />
+			<div className={styles.text}>
+				<h2 style={{ margin: 0, fontWeight: 600 }}>{text}</h2>
 			</div>
 		</div>
 	);
--- src/containers/ShowtimesContainer/index.js
+++ src/containers/ShowtimesContainer/index.js
@@ -1,10 +1,5 @@
-import Header from "../../components/Header";
 import styles from "./ShowtimeContainer.module.scss";
 
 export default function ShowtimeContainer() {
-	return (
-		<div className={styles.container}>
-			<Header text="Showtimes &amp; Tickets" color="red" />
-		</div>
-	);
+	return <div className={styles.container}></div>;
 }
