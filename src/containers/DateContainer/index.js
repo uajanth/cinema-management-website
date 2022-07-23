eabb2f76d3f87adc0d5544ffe6c44209ab3e9026
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-07-23 19:12:00 -0400

    add logic for date related fetching, and the logic for the showtimescontainer

--- src/containers/DateContainer/index.js
+++ src/containers/DateContainer/index.js
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
 import styles from "./DateContainer.module.scss";
+import { isToday, format, addDays, formatISO, startOfToday } from "date-fns";
+import { zonedTimeToUtc } from "date-fns-tz";
 
-export default function DateContainer() {
+export default function DateContainer({ date }) {
+	const [selectedDate, setSelectedDate] = useState(
+		startOfToday(),
+		"yyyy-MM-dd"
+	);
+
+	useEffect(() => {
+		date(format(selectedDate, "yyyy-MM-dd"));
+	}, []);
+
+	const day0 = startOfToday();
+	const day1 = addDays(day0, 1);
+	const day2 = addDays(day0, 2);
+	const day3 = addDays(day0, 3);
+	const day4 = addDays(day0, 4);
+
+	const changeDateHandler = (event) => {
+		event.preventDefault();
+		setSelectedDate(event.target.value);
+		date(event.target.value);
+	};
+
+	const formatDateValue = (date) => {
+		return format(date, "yyyy-MM-dd");
+	};
+
 	return (
 		<div className={styles.container}>
 			<h3>Date</h3>
 			<form>
-				<select id="date">
-					<option value={"test"}>{"Test"}</option>
+				<select id="date" onChange={changeDateHandler} value={selectedDate}>
+					{/* Check every value in select menu if it is in the past using datefns */}
+					{/* Return an error if it is in the past */}
+					<option value={formatDateValue(day0)}>Today</option>
+					<option value={formatDateValue(day1)}>{format(day1, "PPP")}</option>
+					<option value={formatDateValue(day2)}>{format(day2, "PPP")}</option>
+					<option value={formatDateValue(day3)}>{format(day3, "PPP")}</option>
+					<option value={formatDateValue(day4)}>{format(day4, "PPP")}</option>
 				</select>
 			</form>
 		</div>
