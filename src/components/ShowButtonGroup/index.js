80abc9cb490a1d6be39808c3b58a016e66817976
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-07-23 14:45:49 -0400

    fix style sheet import for showbuttongroup component

--- src/components/ShowButtonGroup/index.js
+++ src/components/ShowButtonGroup/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import seatIcon from "../../../public/assets/seat-icon.svg";
-import styles from "./ShowtimeButton.module.scss";
+import styles from "./ShowButtonGroup.module.scss";
 
 export default function ShowButtonGroup({
 	time,
