95fe886b0cedce8ba9aaea0c92c97a01c95ad8ae
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-07-24 16:17:29 -0400

    restructure booking page

--- pages/booking/index.js
+++ pages/booking/index.js
@@ -1,11 +1,16 @@
 import { useRouter } from "next/router";
-import Link from "next/link";
 import { IoExitOutline, IoExit } from "react-icons/io5";
 import styles from "../../styles/Booking.module.scss";
 import Head from "next/head";
 import LogoBar from "../../src/components/LogoBar";
 
 export default function Booking({ showDetails, session }) {
+	const router = useRouter();
+
+	const exitSessionHandler = () => {
+		router.push("/");
+	};
+
 	return (
 		<div>
 			<Head>
@@ -19,50 +24,16 @@ export default function Booking({ showDetails, session }) {
 			<main className={styles.container}>
 				<LogoBar />
 				<div className={styles["link-container"]}>
-					<Link id="exit" href="/" passHref>
-						<a className={styles.link}>
-							Exit
-							<IoExitOutline className={styles.icon} />
-							<IoExit className={styles.icon2} />
-						</a>
-					</Link>
+					<a className={styles.link} onClick={exitSessionHandler}>
+						Exit
+						<IoExitOutline className={styles.icon} />
+						<IoExit className={styles.icon2} />
+					</a>
+				</div>
+				<div className={styles.content}>
+					<h1>Hello world</h1>
 				</div>
 			</main>
 		</div>
 	);
-}
-
-export async function getServerSideProps(context) {
-	try {
-		const showResponse = await fetch(`http://localhost:3000/find-show`, {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ id: context.query.showingId }),
-		});
-		const show = await showResponse.json();
-
-		const sessionResponse = await fetch(
-			"http://localhost:3000/session/retrieve",
-			{
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({
-					showId: context.query.showingId,
-					sessionId: context.query.sessionId,
-				}),
-			}
-		);
-
-		const session = await sessionResponse.json();
-
-		return {
-			props: {
-				showDetails: show[0],
-				session: session,
-			},
-		};
-	} catch (error) {
-		console.log(error);
-		return { props: { showDetails: false, session: false } };
-	}
 }
