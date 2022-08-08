cdced598ee3db09d861d88eeffae9d9d37c137d0
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-08-08 12:33:21 -0400

    add desired behaviour to carousel component

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,21 @@
 import styles from "./Carousel.module.scss";
 
 export default function Carousel() {
+<<<<<<< Updated upstream
 	return <div className={styles.container}></div>;
+=======
+	return (
+		<div className={styles.container}>
+			<Image
+				src={banner}
+				alt="cobra banner"
+				quality={100}
+				layout="fill"
+				objectFit="cover"
+				objectPosition="center"
+				priority
+			/>
+		</div>
+	);
+>>>>>>> Stashed changes
 }
