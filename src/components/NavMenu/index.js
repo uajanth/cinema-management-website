31a68b4506cd21a6a449cfdd38e12786260c2bad
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-09-28 19:08:02 -0400

    Remove nav routes

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -14,28 +14,28 @@ import Link from "next/link";
 export default function NavMenu() {
 	return (
 		<div className={styles.container}>
-			<Link id="tickets" href="/tickets" passHref>
+			<Link id="tickets" href="/" passHref>
 				<a className={styles.link}>
 					<IoTicketOutline className={styles.icon} />
 					<IoTicket className={styles.icon2} />
 					Tickets
 				</a>
 			</Link>
-			<Link id="theatres" href="/theatres" passHref>
+			<Link id="theatres" href="/" passHref>
 				<a className={styles.link}>
 					<IoLocationOutline className={styles.icon} />
 					<IoLocation className={styles.icon2} />
 					Theatres
 				</a>
 			</Link>
-			<Link id="search" href="/search" passHref>
+			<Link id="search" href="/" passHref>
 				<a className={styles.link}>
 					<IoSearchOutline className={styles.icon} />
 					<IoSearch className={styles.icon2} />
 					Search
 				</a>
 			</Link>
-			<Link id="upcoming" href="/upcoming" passHref>
+			<Link id="upcoming" href="/" passHref>
 				<a className={styles.link}>
 					<IoCalendarOutline className={styles.icon} />
 					<IoCalendar className={styles.icon2} />
