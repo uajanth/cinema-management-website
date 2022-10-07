0fc880e70c9743f2f4299f3224a4df3d69e4723d
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-10-07 14:40:05 -0400

    Click on logobar changes route to home page

--- src/components/LogoBar/index.js
+++ src/components/LogoBar/index.js
@@ -1,11 +1,21 @@
 import Image from "next/image";
 import logo from "../../../public/assets/cinemas-logo.png";
 import styles from "./LogoBar.module.scss";
+import { useRouter } from "next/router";
 
 export default function LogoBar() {
+	const router = useRouter();
 	return (
 		<div className={styles.container}>
-			<Image width="120" height="60px" src={logo} alt="" priority />
+			<Image
+				style={{ cursor: "pointer" }}
+				width="120"
+				height="60px"
+				src={logo}
+				alt=""
+				onClick={() => router.replace("/")}
+				priority
+			/>
 		</div>
 	);
 }
