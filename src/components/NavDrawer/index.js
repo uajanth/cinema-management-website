6b450b28950979fea6460b8dbf84d967c04ddb74
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-10-07 17:58:32 -0400

    Implement search for nav drawer

--- src/components/NavDrawer/index.js
+++ src/components/NavDrawer/index.js
@@ -1,18 +1,66 @@
 import styles from "./NavDrawer.module.scss";
 import Drawer from "react-modern-drawer";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 import CinemasLocationCard from "../CinemasLocationCard";
+import { IoSearch } from "react-icons/io5";
 import { useMediaQuery } from "@react-hook/media-query";
+import { useState } from "react";
 
 export default function NavDrawer({ drawer, onClose }) {
+	const [search, setSearch] = useState({ state: false, results: null });
+	const [searchValue, setSearchValue] = useState("");
 	const isGreaterThan320px = useMediaQuery(
 		"only screen and (min-width: 320px)"
 	);
+	const searchRef = useRef() || {};
+	const router = useRouter();
+
+	const searchHandler = async (event) => {
+		setSearch({ state: false, results: null });
+		if (searchRef.current.value.trim().length > 0) {
+			try {
+				const moviesResponse = await fetch(
+					`${process.env.NEXT_PUBLIC_BACKEND}/movies`
+				);
+				if (moviesResponse.ok) {
+					const movies = await moviesResponse.json();
+
+					const filteredMovies = movies.filter((movie) =>
+						movie?.title
+							.toLowerCase()
+							.includes(searchRef.current.value.toLowerCase())
+					);
+
+					setSearch({ state: true, results: filteredMovies });
+					return;
+				}
+				throw new Error();
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
+
+	const closeHandler = () => {
+		setSearch({ state: false, results: null });
+		if (drawer.type === "search") {
+			setSearchValue("");
+		}
+		onClose(drawer.type);
+		return;
+	};
+
+	const selectResultHandler = async (movie) => {
+		await router.push(`/movie/${movie?._id}`);
+		closeHandler();
+	};
 
 	return (
 		<Drawer
 			open={drawer.state}
 			overlayOpacity="0.75"
-			onClose={() => onClose(drawer.type)}
+			onClose={closeHandler}
 			style={{
 				backgroundColor: "#0a0b0d",
 				width: isGreaterThan320px ? "320px" : "100vw",
@@ -22,7 +70,7 @@ export default function NavDrawer({ drawer, onClose }) {
 		>
 			<div className={styles.top}>
 				<h2>{drawer.type === "theatres" ? "Select a theatre" : "Search"}</h2>
-				<button onClick={() => onClose(drawer.type)}>X</button>
+				<button onClick={closeHandler}>X</button>
 			</div>
 			{drawer.type === "theatres" && (
 				<div className={styles.content}>
@@ -58,10 +106,35 @@ export default function NavDrawer({ drawer, onClose }) {
 			{drawer.type === "search" && (
 				<>
 					<div className={styles.content}>
-						<input className={styles.search} type="search" />
-						{/* <div className={styles.box}>
-							<p>Coming Soon</p>
-						</div> */}
+						<div className={styles.search}>
+							<input
+								type="search"
+								ref={searchRef}
+								onKeyDown={(event) => {
+									if (event.key === "Enter") {
+										searchHandler();
+									}
+								}}
+								onChange={(event) => setSearchValue(event.target.value)}
+								value={searchValue}
+								placeholder="Search for a movie"
+							/>
+							<IoSearch
+								className={styles.icon}
+								size="small"
+								onClick={searchHandler}
+							/>
+						</div>
+						{search.state && (
+							<div className={styles.wrapper} style={{ gap: "0.4rem" }}>
+								<h4>{`Results ${search.results.length}`}</h4>
+								{search.results.map((movie, index) => (
+									<p key={index} onClick={() => selectResultHandler(movie)}>
+										{movie.title}
+									</p>
+								))}
+							</div>
+						)}
 					</div>
 				</>
 			)}
