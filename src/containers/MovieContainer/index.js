a20701ba9910c494840365acc6f9a9f4764949ee
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-10-08 17:48:14 -0400

    Remove unncessary ternary operator for movie summary on movie page

--- src/containers/MovieContainer/index.js
+++ src/containers/MovieContainer/index.js
@@ -92,18 +92,7 @@ export default function MovieContainer({ movie }) {
 							{movie?.summary !== "NA" && (
 								<>
 									<h5 className={styles["highlight-title"]}>Summary</h5>
-									<p className={styles["highlight-body"]}>
-										{movie?.summary
-											? movie?.summary
-											: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-									do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-									Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-									irure dolor in reprehenderit in voluptate velit esse cillum
-									dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-									cupidatat non proident, sunt in culpa qui officia deserunt
-									mollit anim id est laborum.`}
-									</p>
+									<p className={styles["highlight-body"]}>{movie?.summary}</p>
 								</>
 							)}
 						</div>
